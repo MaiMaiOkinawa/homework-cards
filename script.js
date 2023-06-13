bf5af4cb13dcf073f5ba595ad93cc1943c89c813
Mai Uchida <maiuchidakishaba@gmail.com>
2023-06-13 20:41:27 +0200

    Modify JS funtions

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-const card1 = document.querySelector("#card1");
-const card2 = document.querySelector("#card2");
-const card3 = document.querySelector("#card3");
-const card4 = document.querySelector("#card4");
+// const data = [
+//   {
+//     day: 100,
+//     homework: "I like diamond.",
+//     image: "https://images.nintendolife.com/5827a29ab6a25/mario.large.jpg",
+//     name: "Mario",
+//   },
+//   {
+//     day: 101,
+//     homework: "I like",
+//     image: "https://images.nintendolife.com/5827a29ab6a25/mario.large.jpg",
+//     name: "Mari",
+//   },
+//   {
+//     day: 101,
+//     homework: "I",
+//     image: "https://images.nintendolife.com/5827a29ab6a25/mario.large.jpg",
+//     name: "Mar",
+//   },
+//   {
+//     day: 101,
+//     homework: "diamond.",
+//     image: "https://images.nintendolife.com/5827a29ab6a25/mario.large.jpg",
+//     name: "M",
+//   },
+// ];
 
-card1.addEventListener("click", function () {
-  card1.classList.toggle("is-flipped");
-});
+// const cards = document.querySelectorAll(".card__inner");
+// const homework = document.querySelectorAll(".hw");
+// const images = document.querySelectorAll(".img");
+// const names = document.querySelectorAll(".name");
 
-card2.addEventListener("click", function () {
-  card2.classList.toggle("is-flipped");
-});
+// let currentData = 0;
 
-card3.addEventListener("click", function () {
-  card3.classList.toggle("is-flipped");
-});
+// function flipCard() {
+//   const currentHomeworkData = data[currentData];
+//   homework.innerText = currentHomeworkData.homework;
+//   this.classList.toggle("is-flipped");
+//   images.src = currentHomeworkData.image;
+//   names.innerText = currentHomeworkData.name;
+// }
 
-card4.addEventListener("click", function () {
-  card4.classList.toggle("is-flipped");
-});
+// cards.forEach((card) => card.addEventListener("click", flipCard));
+
+const cards = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+  this.classList.toggle("is-flipped");
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
